2f7a7024bf7f84ccdb8fe7158b99ab6c1dc51c89
Navivarman <navi200511@gmail.com>
2025-09-10 19:39:52 +0530

    ticket class

---
+++ src/Ticket.java
@@ -0,0 +1,51 @@
+import java.time.Instant;
+public class Ticket {
+    private final String id;
+    private final String plate;
+    private final VehicleType type;
+    private final String spotId;
+    private final Instant entryTime;
+    private Instant exitTime;
+    private double amount;
+    private boolean paid;
+
+    public Ticket(String id, String plate, VehicleType type, String spotId, Instant entryTime, Instant exitTime) {
+        this.id = id;
+        this.plate = plate;
+        this.type = type;
+        this.spotId = spotId;
+        this.entryTime = entryTime;
+        this.paid = false;
+    }
+
+    public void close(Instant exitTime,double amount) {
+        this.exitTime = exitTime;
+        this.amount = amount;
+        this.paid = true;
+    }
+
+    public String getId() {
+        return id;
+    }
+    public String getPlate() {
+        return plate;
+    }
+    public VehicleType getType() {
+        return type;
+    }
+    public String getSpotId() {
+        return spotId;
+    }
+    public Instant getEntryTime() {
+        return entryTime;
+    }
+    public Instant getExitTime() {
+        return exitTime;
+    }
+    public double getAmount() {
+        return amount;
+    }
+    public boolean isPaid() {
+        return paid;
+    }
+}
