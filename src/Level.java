2c3d30a6e040c6253ef8672e31eddcd694c0e73c
Navivarman <navi200511@gmail.com>
2025-09-11 17:17:51 +0530

    level class

---
+++ src/Level.java
@@ -0,0 +1,47 @@
+import java.util.*;
+public class Level {
+    private final int levelNumber;
+    private final Map<String,ParkingSpot> spots;
+
+    public Level(int levelNumber, Map<String,ParkingSpot> spots) {
+        this.levelNumber = levelNumber;
+        this.spots = new HashMap<>();
+    }
+
+    public void addSpot(ParkingSpot spot) {
+        spots.put(spot.getId(), spot);
+    }
+
+    public List<ParkingSpot> getAvailableSpotsByType(SpotType type) {
+        List<ParkingSpot> available = new ArrayList<>();
+        for (ParkingSpot spot : spots.values()) {
+            if (!spot.isOccupied() && spot.getType() == type) {
+                available.add(spot);
+            }
+        }
+        return available;
+    }
+
+    public boolean reserveSpot(String spotId, String ticketId){
+        ParkingSpot spot = spots.get(spotId);
+        if(spot != null){
+            return spot.occupy(ticketId);
+        }
+        return false;
+    }
+
+    public void freeSpot(String spotId){
+        ParkingSpot spot = spots.get(spotId);
+        if(spot != null){
+            spot.free();
+        }
+    }
+
+    public int getLevelNumber() {
+        return levelNumber;
+    }
+
+    public Map<String,ParkingSpot> getSpots() {
+        return spots;
+    }
+}
